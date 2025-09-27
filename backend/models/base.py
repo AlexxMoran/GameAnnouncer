32009d6ca7918f4345c3a92454cabc674a659ae0
Alexey <91380816+AlexxMoran@users.noreply.github.com>
2025-09-27 16:57:54 +0400

    add naming convention

--- backend/models/base.py
+++ backend/models/base.py
@@ -1,10 +1,16 @@
 from datetime import datetime
-from sqlalchemy import DateTime, func
+from sqlalchemy import DateTime, func, MetaData
 from sqlalchemy.orm import (DeclarativeBase, Mapped, mapped_column)
+
+from core.config import settings
 
 class Base(DeclarativeBase):
     __abstract__ = True 
 
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention
+    )
+    
     id: Mapped[int] = mapped_column(primary_key=True)
 
     created_at: Mapped[datetime] = mapped_column(
