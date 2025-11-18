9b170d020abbbda8a2334b8dbb418a36648e1c40
Alexey <91380816+AlexxMoran@users.noreply.github.com>
2025-11-18 17:20:54 +0400

    remove duplicates from router prefixes

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -5,7 +5,7 @@ from .auth import router as auth_router
 
 router = APIRouter()
 router.include_router(auth_router)
-router.include_router(v1_router, prefix=settings.api.v1.prefix)
+router.include_router(v1_router)
 
 
 @router.get("/")
