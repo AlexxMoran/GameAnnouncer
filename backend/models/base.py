4757f68eaed30cc665dc30bca8023165d593159c
Alexey <91380816+AlexxMoran@users.noreply.github.com>
2025-12-21 12:53:12 +0800

    fix imports

--- backend/models/base.py
+++ backend/models/base.py
@@ -3,7 +3,7 @@ from sqlalchemy import DateTime, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from core.config import get_settings
-from core.db import metadata
+from core.db.metadata import metadata
 
 settings = get_settings()
 
