40377cae52885b8549cb34fba4577be3b4c299e5
Владислав Гейер <v.geyer@onpoint.ru>
2026-05-13 19:22:47 +0300

    Some fixes

--- frontend/src/pages/announcement-management/ui/announcement-requests/index.tsx
+++ frontend/src/pages/announcement-management/ui/announcement-requests/index.tsx
@@ -1,11 +1,7 @@
-import type { IOutletContextData } from "@pages/announcement-management/model/types";
 import { Card } from "@shared/ui/card";
 import { observer } from "mobx-react-lite";
 import type { FC } from "react";
-import { useOutletContext } from "react-router";
 
 export const AnnouncementRequests: FC = observer(() => {
-  const outletContext = useOutletContext<IOutletContextData>();
-
   return <Card sx={{ height: "600px" }}></Card>;
 });
